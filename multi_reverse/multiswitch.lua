75e5e3ef3bd2f6685a3a4a91f1eaf1c935ca184d
Shewer Lu <shewer@gmail.com>
2021-08-21 00:17:58 +0800

    add multiswitch method  index toggle on off , modify status next prev
    Signed-off-by: Shewer Lu <shewer@gmail.com>

--- multi_reverse/multiswitch.lua
+++ multi_reverse/multiswitch.lua
@@ -5,6 +5,17 @@
 --
 -- Distributed under terms of the MIT license.
 --
+--      MultiSwitch extend List 
+--  MS=require 'multi_reverse/multiswitch'
+--  ms= MS("a","b","c","d") 
+--  ms() -- return "a"
+--  ms:toggle() return ""
+--  ms:toggle() return "a"
+--  ms:set_status(true) "a"
+--  ms:set_status(false) ""
+--  ms:on() --> set_status(false) 
+--  ms:off() --> set_status(true) 
+--  ms:next() ms:next( 
 MultiSwitch={}
 
 
@@ -16,21 +27,52 @@ end
 -- object method
 function MultiSwitch:reset()
 	self._index=1
+  self._status=true 
 end 
 
-function MultiSwitch:status()
-  return self[ self._index ]
+function MultiSwitch:status(flag)
+  if flag ~= nil  then 
+    self._status= flag and true or false
+  end 
+  return self._status and self[ self._index ] or "" 
+end 
+
+
+function MultiSwitch:toggle()
+  return self:status(not  self._status)
+end 
+
+function MultiSwitch:off()
+  return self:status(false)
+end 
+
+function MultiSwitch:on()
+  return self:status(true)
+end 
+
+
+function MultiSwitch:index(index)
+  if type(index) == "number"  then 
+    self._index = (index) % #self 
+    self._index = self._index == 0 and #self or self._index
+  end 
+  return self._index 
 end 
 function MultiSwitch:next(i)
-  i= i or 1
-  self._index = (self._index + i )% #self
-  self._index = self._index == 0 and #self or self._index
+  self:index( (i or 1) + self._index )
   return self:status()
+
+  --i= i or 1 + self._index 
+  --self._index = (self._index + i )% #self
+  --self._index = self._index == 0 and #self or self._index
+  --return self:status()
 end
 function MultiSwitch:prev(i)
-  i= -( i or 1)
-  self:next(i)
-  return self:status()
+  self:index( -(i or 1) + self._index )
+  return self:status() 
+  --i= -( i or 1)
+  --self:next(i)
+  --return self:status()
 end 
 
 
@@ -43,7 +85,7 @@ MultiSwitch.__shl=List.__shl    --     a=List(1,2,3) ; a  << MultiSwitch(9,8)<< 
 								--     a=MultiSwitch(1,2) ; a << List(3,4) << 1 << 2 <<{5,6} 
 								--         a= {1,2,3,4,1,2,5,6} (Multiswitch)
 MultiSwitch.__eq=List.__eq      --     a=List(1,2) ;b=MultiSwitch(1,2) ;c={1,2} ; a == b ; b== a b==c ;a == c
-
+MultiSwitch.__call=MultiSwitch.status --   obj() return obj:status() 
 
 
 MultiSwitch.__index=MultiSwitch 
--- test/multiswitch_test.lua
+++ test/multiswitch_test.lua
@@ -13,29 +13,29 @@ local function addrcheck(obj1,obj2)
 	return tostring(obj1) == tostring(obj2) 
 end 
 local List_=require'tools/list'
-local List=require'multi_reverse/multiswitch'
-TestList={}
+local MList =require'multi_reverse/multiswitch'
+TestMSList={}
 
-function TestList:setUp()
-  self.list_read_only = List(1,2,3,4)
+function TestMSList:setUp()
+  self.list_read_only = MList(1,2,3,4)
 end
 
-function TestList:testInit()
-  lu.assertEquals(#List(), 0)
-  lu.assertEquals(#List(1,2,3,4), 4)
-  lu.assertEquals(#List({1,2,3,4}), 4)
-  lu.assertEquals(#List({1,2,3,4},2,3), 3)
+function TestMSList:testInit()
+  lu.assertEquals(#MList(), 0)
+  lu.assertEquals(#MList(1,2,3,4), 4)
+  lu.assertEquals(#MList({1,2,3,4}), 4)
+  lu.assertEquals(#MList({1,2,3,4},2,3), 3)
 end 
-function TestList:test_operation__eq()
-  local list=List(1,2,3,4)
-  lu.assertTrue( List(1,2,3,4) == {1,2,3,4}) 
-  lu.assertTrue( List(1,2,3,4) == List({1,2,3,4})) 
-  lu.assertFalse( List(1,2,3,4) == {1,2,3,5} )
-  lu.assertFalse( List(1,2,3,4) == {1,2,3} )
+function TestMSList:test_operation__eq()
+  local list=MList(1,2,3,4)
+  lu.assertTrue( MList(1,2,3,4) == {1,2,3,4}) 
+  lu.assertTrue( MList(1,2,3,4) == MList({1,2,3,4})) 
+  lu.assertFalse( MList(1,2,3,4) == {1,2,3,5} )
+  lu.assertFalse( MList(1,2,3,4) == {1,2,3} )
 end 
-function TestList:test_operation__add()
-  local l1=List(1,2)
-  local l2=List(3,4)
+function TestMSList:test_operation__add()
+  local l1=MList(1,2)
+  local l2=MList(3,4)
   local l3= l1 + l2 + {5,6}  
   lu.assertTrue(l3=={1,2,3,4,5,6})
   lu.assertFalse(addrcheck(l3,l1))
@@ -43,8 +43,8 @@ function TestList:test_operation__add()
   lu.assertEquals( l3:class(),l2:class() )
   lu.assertTrue(addrcheck(l3:class(),l2:class()))
 end 
-function TestList:test_operation__shl()
-  local l1=List(1,2)
+function TestMSList:test_operation__shl()
+  local l1=MList(1,2)
   local l2=l1
   local l3=l1 << {3,4}  << {5,6}
   lu.assertTrue(l1=={1,2,3,4,5,6})
@@ -52,13 +52,13 @@ function TestList:test_operation__shl()
   lu.assertTrue(addrcheck(l1,l2))
   lu.assertTrue(addrcheck(l3:class(),l2:class()))
   lu.assertTrue(addrcheck(l3:class(),l2:class()))
-  l1=List(1,2)
-  local l3= l1 << List(3,4)+ {5,6}  <<  7<< 8
+  l1=MList(1,2)
+  local l3= l1 << MList(3,4)+ {5,6}  <<  7<< 8
   lu.assertTrue(l1=={1,2,3,4,5,6,7,8})
-  _= List() << 2 << 4 << 33
+  _= MList() << 2 << 4 << 33
 end 
-function TestList:test_clear()
-	local l1=List(1,2,3,4) 
+function TestMSList:test_clear()
+	local l1=MList(1,2,3,4) 
 	local l2 = l1
 	local l3 = l1:clear() 
 	lu.assertTrue(l1,{})
@@ -67,8 +67,8 @@ function TestList:test_clear()
 	lu.assertEquals(tostring(l1),tostring(l3))
 end 
 
-function TestList:test_insert_at()
-	local l1=List(1,2,3,4) 
+function TestMSList:test_insert_at()
+	local l1=MList(1,2,3,4) 
 	local l2 = l1
 	local l3 = l1:insert_at(1,0)
 	lu.assertTrue(l1== {0,1,2,3,4}) -- unshift
@@ -80,8 +80,8 @@ function TestList:test_insert_at()
 	lu.assertTrue(l1== {0,1,1.5,2,3,4,5})
 	lu.assertTrue(addrcheck(l1,l2) and addrcheck(l1,l3))
 end 
-function TestList:test_push()
-	local l1=List(1,2,3,4) 
+function TestMSList:test_push()
+	local l1=MList(1,2,3,4) 
 	local l2 = l1
 	local l3 = l1:push(5)
 	lu.assertTrue(l1== {1,2,3,4,5}) -- unshift
@@ -89,14 +89,14 @@ function TestList:test_push()
 	local l3 = l1:push(6) --- push
 	lu.assertTrue(l1== {1,2,3,4,5,6})
 	lu.assertTrue(addrcheck(l1,l2) and addrcheck(l1,l3))
-	local l1=List() 
+	local l1=MList() 
 	local l2 = l1
 	local l3 = l1:push(1)
 	lu.assertTrue(l1== {1}) -- unshift
 	lu.assertTrue(addrcheck(l1,l2) and addrcheck(l1,l3))
 end 
-function TestList:test_unshif()
-	local l1=List(1,2,3,4) 
+function TestMSList:test_unshif()
+	local l1=MList(1,2,3,4) 
 	local l2 = l1
 	local l3 = l1:unshift(5)
 	lu.assertTrue(l1== {5,1,2,3,4}) -- unshift
@@ -104,7 +104,7 @@ function TestList:test_unshif()
 	local l3 = l1:unshift(6) --- push
 	lu.assertTrue(l1== {6,5,1,2,3,4})
 	lu.assertTrue(addrcheck(l1,l2) and addrcheck(l1,l3))
-	local l1=List() 
+	local l1=MList() 
 	local l2 = l1
 	local l3 = l1:unshift(1)
 	lu.assertTrue(l1== {1}) -- unshift
@@ -114,8 +114,8 @@ function TestList:test_unshif()
 	lu.assertTrue(addrcheck(l1,l2) and addrcheck(l1,l3))
 end 
 -- obj:remove_at(index(1)  return elm, self  
-function TestList:test_remove_at()
-	local l1=List(1,2,3,4)
+function TestMSList:test_remove_at()
+	local l1=MList(1,2,3,4)
 	local l2=l1 
 	-- index out of range  return nil
 	local elm,l3= l1:remove_at(5) 
@@ -149,7 +149,7 @@ function TestList:test_remove_at()
 	lu.assertEquals(elm,1)
 	lu.assertTrue(addrcheck(l1,l2) and addrcheck(l1,l3))
 
-	local l1=List(1,2,3,4)
+	local l1=MList(1,2,3,4)
 	local l2=l1 
 	local elm,l3= l1:remove_at(-2) 
 	lu.assertTrue(l1== {1,2,4}) -- unshift
@@ -168,8 +168,8 @@ function TestList:test_remove_at()
 
 end 
 -- -->  obj:remove_at(1) 
-function TestList:test_shift()
-	local l1=List(1,2,3)
+function TestMSList:test_shift()
+	local l1=MList(1,2,3)
 	local l2=l1
 
 	local elm,l3= l1:shift(4) -- don't care  
@@ -191,8 +191,8 @@ function TestList:test_shift()
 
 end 
 --  obj:remove_at(-1) 
-function TestList:test_pop()
-	local l1=List(1,2,3)
+function TestMSList:test_pop()
+	local l1=MList(1,2,3)
 	local l2=l1
 
 	local elm,l3= l1:pop(4) -- don't care  
@@ -213,16 +213,16 @@ function TestList:test_pop()
 	lu.assertTrue(addrcheck(l1,l2) and addrcheck(l1,l3))
 	
 end 
-function TestList:test_concat()
-	local l1=List(1,2,3,4)
+function TestMSList:test_concat()
+	local l1=MList(1,2,3,4)
 	lu.assertEquals(l1:concat(),"1234")
 	lu.assertEquals(l1:concat(","),"1,2,3,4")
-	lu.assertEquals(List():concat(","),"")
-	lu.assertEquals(List():concat(),"")
+	lu.assertEquals(MList():concat(","),"")
+	lu.assertEquals(MList():concat(),"")
 end 
 
-function TestList:test_each()
-	local l1=List(1,2,3,4)
+function TestMSList:test_each()
+	local l1=MList(1,2,3,4)
 	local l2=l1
 	local count=0
 	local sum=0
@@ -231,8 +231,8 @@ function TestList:test_each()
 	lu.assertEquals( count, 4)
 	lu.assertEquals(sum, 10)
 end 
-function TestList:test_each_with_index()
-	local l1=List(1,2,3,4)
+function TestMSList:test_each_with_index()
+	local l1=MList(1,2,3,4)
 	local l2=l1
 	local count=0
 	local sum=0
@@ -252,8 +252,8 @@ function TestList:test_each_with_index()
 
 end 
 
-function TestList:test_map()
-	local l1 = List(1,2,3,4) 
+function TestMSList:test_map()
+	local l1 = MList(1,2,3,4) 
 	local l2 = l1:map() --  clone obj
 	lu.assertEquals(l1,l2)
 	lu.assertTrue(l2,{1,2,3,4})
@@ -264,15 +264,15 @@ function TestList:test_map()
 
 	lu.assertTrue(l1:map(function(elm) return elm end ) == {1,2,3,4} )
 
-	lu.assertEquals(l1:map(function(elm) return {elm ,elm} end  ) , {{1,1},{2,2},{3,3},{4,4},_index=1} )
+	lu.assertEquals(l1:map(function(elm) return {elm ,elm} end  ) , {{1,1},{2,2},{3,3},{4,4},_index=1, _status=true} )
 	lu.assertTrue(l1:map(function(elm) return elm * 2 end ) == {2,4,6,8} )
 
 	--  lua  nil  特性
 	lu.assertTrue(l1:map(function(elm,i) return (elm & 1)==1  and elm or nil end ) == {1,3} )
 end 
 
-function TestList:test_map_with_index()
-	local l1 = List(1,2,3,4) 
+function TestMSList:test_map_with_index()
+	local l1 = MList(1,2,3,4) 
 	local l2 = l1:map_with_index() --  clone obj
 	lu.assertEquals(l1,l2)
 	lu.assertTrue(l2=={1,2,3,4})
@@ -283,15 +283,15 @@ function TestList:test_map_with_index()
 
 	lu.assertTrue(l1:map_with_index(function(elm) return elm end ) == {1,2,3,4} )
 	lu.assertTrue(l1:map_with_index(function(elm,i) return elm end ) == {1,2,3,4} )
-	lu.assertEquals(l1:map_with_index(function(elm,i) return (i & 1)== 1  and elm end ) ,{1,false,3,false,_index=1} )
-	lu.assertEquals(l1:map(function(elm) return {elm ,elm} end  ) , {{1,1},{2,2},{3,3},{4,4},_index=1} )
+	lu.assertEquals(l1:map_with_index(function(elm,i) return (i & 1)== 1  and elm end ) ,{1,false,3,false,_index=1, _status=true} )
+	lu.assertEquals(l1:map(function(elm) return {elm ,elm} end  ) , {{1,1},{2,2},{3,3},{4,4},_index=1, _status=true} )
 	lu.assertTrue(l1:map_with_index(function(elm,i) return elm * 2 end ) == {2,4,6,8} )
 
 	--  lua  nil  特性
 	lu.assertTrue(l1:map_with_index(function(elm,i) return (i & 1)== 1  and elm or nil end ) == {1,3} )
 end 
-function TestList:test_select()
-	local l1 = List(1,2,3,4) 
+function TestMSList:test_select()
+	local l1 = MList(1,2,3,4) 
 	local l2 = l1:select(function(elm) return true end ) --  clone obj
 	lu.assertEquals(l1,l2)
 	lu.assertTrue(l2=={1,2,3,4})
@@ -307,8 +307,8 @@ function TestList:test_select()
 
 end 
 
-function TestList:test_select_withe_index()
-	local l1 = List(1,2,3,4) 
+function TestMSList:test_select_withe_index()
+	local l1 = MList(1,2,3,4) 
 	local l2 = l1:select_with_index(function(elm) return true end ) --  clone obj
 	lu.assertEquals(l1,l2)
 	lu.assertTrue(l2=={1,2,3,4})
@@ -323,13 +323,47 @@ function TestList:test_select_withe_index()
     lu.assertTrue(l1:select_with_index(function(elm,i) return i >2  end )=={3,4})
 
 end 
+--- test MultiSwitch 
+function TestMSList:test_status()
+  local l1=MList("a","b","c","d" )
+  lu.assertEquals(l1(), "a")
+  lu.assertEquals(l1:status(), "a")
+  lu.assertEquals(l1:status(false), "")
+  lu.assertEquals(l1(), "")
+  lu.assertEquals(l1:status(true), "a")
+  lu.assertEquals(l1(), "a")
+  lu.assertEquals(l1:off(), "")
+  lu.assertEquals(l1:on(), "a")
+  lu.assertEquals(l1:toggle(), "")
+  lu.assertEquals(l1:toggle(), "a")
+end 
+
+function TestMSList:test_chang_index()
+  local l1=MList("a","b","c","d" )
+  lu.assertEquals(l1:next(),  "b")
+  lu.assertEquals(l1(), "b")
+  lu.assertEquals(l1:status(), "b")
+  lu.assertEquals(l1:next(1),  "c")
+  lu.assertEquals(l1:index() , 3)
+  lu.assertEquals(l1:index(1), 1)
+  lu.assertEquals(l1:index(0), 4)
+  lu.assertEquals(l1:index(4), 4)
+  lu.assertEquals(l1:index(-4), 4)
+  lu.assertEquals(l1:index(-1), 3)
+  lu.assertEquals(l1:index(), 3)
+  lu.assertEquals(l1:next(2), "a")
+  lu.assertEquals(l1:next(-2), "c")
+  lu.assertEquals(l1:prev(2), "a")
+  lu.assertEquals(l1:prev(-2), "c")
 
 
+end
 
 
 assert(  lu.LuaUnit )
 assert(  lu.LuaUnit.run)
 --os.exit( lu.LuaUnit.run() )
 
-return TestList
+
+return TestMSList
 
