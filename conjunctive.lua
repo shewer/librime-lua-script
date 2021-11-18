43c8045f465d972384fd776151ec65837899c557
Shewer Lu <shewer@gmail.com>
2021-11-18 16:14:34 +0800

    update
    Signed-off-by: Shewer Lu <shewer@gmail.com>

--- conjunctive.lua
+++ conjunctive.lua
@@ -25,53 +25,10 @@ recognizer/patterns/conjunctive:  "^" .. pattern_str  觸發 tag
 local List=require 'tools/list'
 function __LINE__(n) n=n or 2 return debug.getinfo(n, 'l').currentline end
 
-local function get_tags(config,path)
-  local list=List()
-  for i=0,config:get_list_size(path) -1 do
-    list:push( config:get_string(path .. "/@" .. i ) )
-  end
-  return list
-end
-
-local F={}
-function F.init(env)
-  local context=env.engine.context
-  local config=env.engine.schema.config
-
-  env.match_all = config:is_null(env.name_space .. "/tags")
-  env.tags = env.match_all and List() or get_tags(config,path )
-  env.reject_tags=  List{}
-  env.match_type={history=true}
-
-end
-
-function F.fini(env)
-end
-
-function F.func(tran,env)
-  local uniquify_key={}
-  for cand in tran:iter() do
-    if env.match_type[cand.type] then
-      yield( cand )
-    elseif  not uniquify_key[cand.text] then
-      uniquify_key[cand.text]  = true
-      yield(cand)
-    end
-  end
-end
-function F.tags_match(seg,env)
-  if env.match_all then
-    env.reject_tags:each(function(elm) seg:has_tag(elm) end  )
-  else
-
-  end
-  return true
-
-end
 
 
 -- 使用者常用詞
-_HISTORY=List{
+local _HISTORY=List{
   "發性進行性失語",
   "發性症狀",
   "發性行性失語症狀",
@@ -84,11 +41,12 @@ _HISTORY=List{
 }
 
 -- user define data
-local pattern_str="~~"
-local lua_tran_ns="conjunctive"
-local dict_file= 'essay.txt'
+local pattern_str ="~~"
+local rec_pattern = ("^%s%s$"):format(pattern_str,"[A-Z<>~]*") 
+local lua_tran_ns = "conjunctive"
+local dict_file = 'essay.txt'
 --dict_file= '/usr/share/rime-data/essay.txt'  -- debug
-local switch_key="F11"
+local switch_key ="F11"
 
 local path_ch= package.config:sub(1,1)
 
@@ -101,6 +59,7 @@ local M={}
 _G[lua_tran_ns]=M
 function M.init(env)
   env.dict= _dict or Dict("essay.txt")
+
   env.history=""
   env.history_back=""
 
@@ -122,7 +81,7 @@ function M.init(env)
     if update_commit then
       env.history = env.history .. ctx:get_commit_text() or ""
       env.history = env.history:utf8_sub(-10)
-      print( env.history, ctx.input , ctx:get_commit_text() )
+      --print( env.history, ctx.input , ctx:get_commit_text() )
 
       env.history_commit=  not env.dict:empty(env.history)
     end
@@ -143,55 +102,40 @@ function M.fini(env)
   env.update_connect:disconnect()
 end
 
-local function set_history(input,seg,env)
-
-  local str = input:match("^" .. pattern_str .. "(.*)$") or ""
-  if str == "C" then
-    yield( Candidate("history", seg.start, seg._end, "", "--清除(" .. env.history .. ")" ))
-    yield( Candidate("history", seg.start, seg._end, "", env.history_back.. "--還原" ))
-    return env.history
-  end
-  if str == "B" then
-    yield( Candidate("history", seg.start, seg._end, "", env.history_back.. "--還原" ))
-    return env.history
-  end
-  if str == "H" then
-    for i=1,#_HISTORY do
-      local history_str=_HISTORY[i]
-      if history_str and history_str:len()>0 then
-        yield( Candidate("history", seg.start, seg._end,  history_str ,"" ..  "--選用" ))
-      end
-    end
-    return env.history
-  end
-  if str:match("[<>~]+") then
-    local si=1
-    local ei=1
-    for i=1,#str do
-      local c = str:sub(i,i)
-      ei =  c:match("[~<]") and ei - 1 or ei
-      si =  c:match("[>]") and si + 1 or si
-    end
-    ei = ei < 0  and ei or nil
-    local history_str = env.history:utf8_sub(si,ei) or ""
-    yield( Candidate("history", seg.start, seg._end, "", history_str.. "--修改" ))
-
-    return history_str
-  end
-
-  return env.history
-
-end
 
 function M.func(input,seg,env)
   local context = env.engine.context
+  local history= env.history
+
   if context:get_option(lua_tran_ns) then return end  -- false: enable true: disable
   if not seg:has_tag(lua_tran_ns)  then return end
 
-  -- change env.history string
-  local history_str = set_history(input,seg,env)
+  local active_input = input:match("^".. pattern_str .. "(.*)$")
+  if active_input == nil then return end
 
-  for w ,wt in env.dict:reduce_iter(history_str) do
+  if active_input == "C" then
+    yield( Candidate("history", seg.start, seg._end, "", "--清除 (" .. history .. ")" ))
+    yield( Candidate("history", seg.start, seg._end, "", env.history_back.. "--還原" ))
+
+  elseif active_input == "B" then
+    yield( Candidate("history", seg.start, seg._end, "", env.history_back.. "--還原" ))
+
+  elseif active_input == "H" then
+    _HISTORY:each(function(elm)
+      yield( Candidate("history", seg.start, seg._end, elm, "--自選" ))
+    end)
+
+  elseif active_input:match("[<>~]+") then
+    if #active_input >1 then
+      active_input=active_input:sub(2)
+      local si= #active_input:gsub("[^>]","") +1
+      local ei= - #active_input:gsub("[^<~]","")
+      history = history:utf8_sub(si,ei)
+    end
+    yield( Candidate("history", seg.start, seg._end, "", history .. "--修改" ))
+  end
+
+  for w ,wt in env.dict:reduce_iter(history) do
     yield( Candidate( lua_tran_ns , seg.start,seg._end, w, "(聯)") )
   end
 end
@@ -210,6 +154,9 @@ local function print_config(config)
   List({"processors","segmentors","translators","filters"})
   :map(function(elm) return "engine/" .. elm end )
   :each(function(elm) list_print(config,elm) end)
+
+  local pattern_p= "recognizer/patterns/" .. lua_tran_ns
+  puts ( pattern_p .. ":\t" .. ( config:get_string(pattern_p ) or "") )
 end
 
 -- option conjunctive enable(false ) disable(true)
@@ -227,28 +174,38 @@ local function add_keybind(config,keybind)
   end
 end
 
+local function find_index(config,path,after,before)
+  local index, after_i,before_i= nil,nil,nil
+  for i=0,config:get_list_size(path) -1 do 
+    local str= config:get_string(path .. "/@" .. i )
+    after_i =  after_i or (  str:match(after) and i or nil )
+    before_i = before_i or ( str:match(before) and i or nil )
+  end 
 
-local function add_component_tran(config,component_str)
-  component_str=  component_str or "lua_translator@" .. lua_tran_ns
-  local punct_str= "punct_translator"
-  local echo_str= "echo_translator"
-  local punct_i,echo_i
-  local tran_path= "engine/translators"
-  -- find index  of echo and punct
-  local trans_list_size = config:get_list_size(tran_path)
-  for i=0,trans_list_size-1 do
-    local tran= config:get_string( ("%s/@%s"):format(tran_path, i) ) or ""
-    echo_i = ( not echo_i and  tran:match(echo_str) ) and i or echo_i
-    punct_i = ( not punct_i and tran:match(punct_str) ) and i or punct_i
-    -- conjunctive already in translators
-    if tran:match(component_str) then return end
-  end
-  -- insert  lua_translator  of conjunctive into translators
-  local index = echo_i and  echo_i +1 or 0  -- index < echo_i
-  index = punct_i  and index >=punct_i and index or punct_i  -- index >= punct_i
-  config:set_string( tran_path .. "/@before " .. index, component_str )
+  index = after_i and  after_i  or 0  -- index < echo_i
+  index = before_i  and index >=before_i and before_i or index +1   -- index >= punct_i
+  return index, after_i,before_i
+end 
+local function find_component(config,path,name)
+  for i=0,config:get_list_size(path) -1 do 
+    if config:get_string(path .. "/@" .. i ):match(name) then 
+      return true
+    end 
+  end 
+end 
+-- add lua_translator@conjunctive between echo_translator and punct_translator
+local function add_component_tran(config,name)
+  name=  name or "lua_translator@" .. lua_tran_ns
+  local path= "engine/translators"
+  local after,before= "echo_translator","punct_translator"
+  
+  if not find_component(config,name) then 
+    local index, after_i , before_i = find_index(config,path,after,before)
+    config:set_string( path .. "/@before " .. index, name ) 
+  end 
 end
 
+-- repleace uniquifier to lua_filter@uniquifier 
 local function replase_filter(config)
   local path= "engine/filters"
   local refilter= "uniquifier"
@@ -260,32 +217,101 @@ local function replase_filter(config)
   end
 end
 
-local P={}
+local function get_tags(config,path)
+  local list=List()
+  for i=0,config:get_list_size(path) -1 do
+    list:push( config:get_string(path .. "/@" .. i ) )
+  end
+  return #list >0 and list or nil 
+end
 
-function P.init(env)
+-- lua_filter@uniquifier
+local F={}
+function F.init(env)
+  local context=env.engine.context
   local config=env.engine.schema.config
+  local path= env.name_space .. "/tags"
+  env.tags = get_tags(config,path )
+  env.unmatch_type={history=true}
+end
 
+function F.fini(env)
+end
+
+function F.tags_match(seg,env)
+  if not env.tags  then  --  
+    return env.unmatch_tag and not env.reject_tags:find(function(elm) return seg:has_tag(elm) end )
+  else
+    return not not env.tags:find(function(elm) return seg:has_tag(elm) end )
+  end
+end
+
+function F.func(tran,env)
+  local uniquify_key={}
+  for cand in tran:iter() do
+    if env.match_type[cand.type] then
+   ;   yield( cand )
+    elseif  not uniquify_key[cand.text] then
+      uniquify_key[cand.text]  = true
+      yield(cand)
+    end
+  end
+end
+
+-- add lua_translator@conjunctive , lua_filter@uniquifier 
+--
+local function components(env)
+  local config=env.engine.schema.config
+  
   -- set module  "conjunctive"
   _G[lua_tran_ns]= _G[lua_tran_ns] or M
   _G["uniquifier"]= F -- require 'component/uniquify'
 
-  -- set alphabet string
-  env.alphabet= config:get_string("speller/alphabet") or "zyxwvutsrqponmlkjihgfedcba"
 
-  config:set_string("recognizer/patterns/" .. lua_tran_ns ,
-  "^" .. pattern_str .."[BCH<>~]*$" )
+  config:set_string("recognizer/patterns/" .. lua_tran_ns , rec_pattern) 
 
   -- register keybinder {when: "always",accept: switch_key, toggle: conjunctive }
   add_keybind(config, {when= "always", accept= switch_key, toggle= lua_tran_ns } )
   -- add lua_translator after echo_translator before punct_translator
-  add_component_tran(config, "lua_translator@" .. lua_tran_ns )
+  local path= "engine/translators"
+  local name= "lua_translator@" .. lua_tran_ns 
+  local after,before= "echo_translator","punct_translator" 
+  if not find_component( config, path, name ) then 
+    local index,after_i,before_i = find_index(config,path,after,before)
+    config:set_string(path .. "/@before " .. index , name )
+  end 
+  --add_component_tran(config, "lua_translator@" .. lua_tran_ns )
   -- replease uniquifire
   replase_filter(config)
   -- print compoments
   print_config(config)
-  local pattern_p= "recognizer/patterns/" .. lua_tran_ns
-  print( pattern_p , config:get_string(pattern_p ) )
+end
+local P={}
 
+function P.component(env,name)
+  name = name or  "lua_processor@" .. lua_tran_ns .. "_proc"
+  _G[lua_tran_ns .. "_proc"] = P 
+  local config=env.engine.schema.config
+  -- between  processor_init and recognizer 
+  local path= "engine/processors"
+  if find_component(config,path,name) then 
+    return false
+  else 
+    local after, before = "lua_processor@processor_init", "recognizer"
+    local index,after_i,before_i=find_index(config,path,after,before)
+    -- insert  lua_translator  of conjunctive into translators
+    local index,after_i, before_i = find_index(config,path,after,before)
+    config:set_string( path .. "/@before " .. index, name )
+    print_config(config)
+    return true
+  end 
+end 
+
+function P.init(env)
+  local config= env.engine.schema.config
+  components(env)
+  -- set alphabet string
+  env.alphabet= config:get_string("speller/alphabet") or "zyxwvutsrqponmlkjihgfedcba"
 end
 
 function P.fini(env)
@@ -293,6 +319,7 @@ end
 
 function P.func(key, env)
   local Rejected,Accepted,Noop= 0,1,2
+  
   local context = env.engine.context
 
   -- true(1) : disable  false(0) enable
@@ -307,14 +334,9 @@ function P.func(key, env)
   if not context:is_composing() and  ascii == "~" then
     context.input= pattern_str
     return Accepted
-  elseif context.input:match("^" .. pattern_str) then
-    -- "~~~" 聯想工作狀態
-    -- lua_translator@conjunctive 處理 env.history
-    if ascii:match("^[BCH<>~]$") then
-      context:push_input(ascii)
-      return Accepted
-    end
-    -- 中斷聯想
+  end 
+  -- 中斷聯想
+  if  context.input:match("^" .. pattern_str) then
     if ascii:match("^[" .. env.alphabet .. "]$" ) then
       context:clear()
       return Noop
