d6be5fb6c2ebcca7fb850c2db0d69a17be82fecb
Shewer Lu <shewer@gmail.com>
2022-01-27 21:00:43 +0800

    增加優先載入 wordninja-rs
    Signed-off-by: Shewer Lu <shewer@gmail.com>

--- english/english_tran.lua
+++ english/english_tran.lua
@@ -5,10 +5,30 @@ local D01= nil
 
 local English_dict= require 'tools/english_dict'
 
+
 local function njload()
-  local nj= require 'tools/wordninja'
-  nj.init()
-  nj.test()
+  -- try to load wordninja-lua 
+  -- https://github.com/BlindingDark/wordninja-rs-lua
+  -- cp wordninja.so  <user_data_dir>/lua/plugin
+  local cpath=package.cpath
+  local df= cpath:match("so") or cpath:match("dll") or cpath:match("dylib") 
+  local path= (";/lua/plugin/?." .. df):gsub("/",package.config:sub(1,1) )
+  package.cpath=package.cpath ..  path
+
+  local ok,res= pcall(require, "wordninja" )
+  local nj= ok and res or nil
+  puts(CONSOLE,nj,ok,ok and res)
+  if not nj then
+    nj= require 'tools/wordninja'
+    nj.init()
+  end
+  do
+    local t1= os.clock()
+    local ss="WethepeopleoftheunitedstatesinordertoformamoreperfectunionestablishjusticeinsuredomestictranquilityprovideforthecommondefencepromotethegeneralwelfareandsecuretheblessingsoflibertytoourselvesandourposteritydoordainandestablishthisconstitutionfortheunitedstatesofAmerica"
+    local ss_res="We the people of the united states in order to form a more perfect union establish justice in sure domestic tranquility provide for the common defence promote the general welfare and secure the blessings of liberty to ourselves and our posterity do ordain and establish this constitution for the united states of America"
+    local s= nj.split(ss)
+    puts(CONSOLE,"rust wordnanja tes",ss ,"\n", s ,"\n",os.clock() - t1,  s== ss_res )
+  end
   return nj
 end
 
@@ -37,6 +57,8 @@ local T={}
 T._eng_dict=T._eng_dict or English_dict("english_tw.txt")
 T._nj= T._nj or njload()
 T._ext_dict= T.ext_dict or load_ext_dict("ext_dict.txt")
+puts(CONSOLE, __FILE__(),__LINE__(), package.cpath,"\n\n",  package.path )
+
 function T.init(env)
   local config= env.engine.schema.config
   env.tag= config:get_string(env.name_space .. "/tag") or English
@@ -62,7 +84,7 @@ function T.func(inp,seg,env)
 
   local commit= input:match("^[%a%_%.%']+$")  and T._nj.split(input) or {}
   if #commit > 1 then
-    yield( Candidate(Ninja, seg.start,seg._end, table.concat(commit," "), "[ninja]"))
+    yield( Candidate(Ninja, seg.start,seg._end, commit, "[ninja]"))
   end
 
   -- 使用 context.input 杳字典 type "english"
@@ -76,7 +98,8 @@ function T.func(inp,seg,env)
   end
   -- 使用 ninja 最後一佪字查字典 type "ninja"
   if #commit > 1 then
-    local n_word= commit[#commit]
+    --local n_word= commit[#commit]
+    local n_word= commit:match(".* (.+)$") 
 
     for w in T._eng_dict:iter(n_word) do
       -- seg.start =   seg.end - #m_word
