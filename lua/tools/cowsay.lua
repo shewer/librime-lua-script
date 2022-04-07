ccbcfb43a8248d2ec7a760b0ce0be8cd1bee814e
Shewer Lu <shewer@gmail.com>
2022-04-07 11:12:05 +0800

    update
    Signed-off-by: Shewer Lu <shewer@gmail.com>

--- lua/tools/cowsay.lua
+++ lua/tools/cowsay.lua
@@ -1,7 +1,6 @@
 require 'tools/string'
 local List = require 'tools/list'
 local cows={   }
-cows.name={'cow','armadillo','bearface','cat','cat2'}
 cows.cow=[[
 %s
 %s
@@ -33,7 +32,7 @@ cows.bearface= [[
     : (\\ ". _......_ ." /) :
      '.    `        `    .'
       /'   _        _   `\\
-     /     $eye}      {$eye     \\
+     /     e}      {e     \\
     |       /      \\       |
     |     /'        `\\     |
      \\   | .  .==.  . |   /
@@ -74,19 +73,97 @@ cows.cat2 = [[
          //_// ___/
              \\_) 
 ]]
+
+cows.eye= [[
+    $thoughts
+     $thoughts
+                                   .::!!!!!!!:.
+  .!!!!!:.                        .:!!!!!!!!!!!!
+  ~~~~!!!!!!.                 .:!!!!!!!!!UWWW\$\$\$
+      :\$\$NWX!!:           .:!!!!!!XUWW\$\$\$\$\$\$\$\$\$P
+      \$\$\$\$\$##WX!:      .<!!!!UW\$\$\$\$"  \$\$\$\$\$\$\$\$#
+      \$\$\$\$\$  \$\$\$UX   :!!UW\$\$\$\$\$\$\$\$\$   4\$\$\$\$\$*
+      ^\$\$\$B  \$\$\$\$\\     \$\$\$\$\$\$\$\$\$\$\$\$   d\$\$R"
+        "*\$bd\$\$\$\$      '*\$\$\$\$\$\$\$\$\$\$\$o+#"
+             """"          """""""
+]]
+
+cows.kitten= [[
+%s
+%s
+%s
+       ("`-'  '-/") .___..--' ' "`-._
+         ` *_ *  )    `-.   (      ) .`-.__. `)
+         (_Y_.) ' ._   )   `._` ;  `` -. .-'
+      _.. `--'_..-_/   /--' _ .' ,4
+   ( i l ),-''  ( l i),'  ( ( ! .-'
+ ]]
+cows.tux= [[
+%s
+%s
+%s
+        .--.
+       |o_o |
+       |:_/ |
+      //   \\ \\
+     (|     | )
+    /'\\_   _/`\\
+    \\___)=(___/
+
+]]
+cows.koala = [[
+%s
+%s
+%s
+             .
+     .---.  //
+    Y|o o|Y//
+   /_(i=i)K/
+   ~()~*~()~
+    (_)-(_)
+
+     Darth
+     Vader
+     koala
+]]
+
+cows.stimpy = [[
+%s
+%s
+%
+
+         .    _  .
+         |\\_|/__/|
+       / / \\/ \\  \\
+      /__|O||O|__ \\
+     |/_ \\_/\\_/ _\\ |
+     | | (____) | ||
+     \\/\\___/\\__/  //
+     (_/         ||
+      |          ||
+      |          ||\\
+       \\        //_/
+        \\______//
+       __ || __||
+      (____(____)
+]]
 local function cgumirs(y)
    local y4 = y % 4 == 0
    local y100 = y % 100 == 0
    local y400 = y % 400 ==0
    return y4 and not y100 or y400
 end
+
+local cowsay_list={}
+for k,v in next,cows do table.insert(cowsay_list,k) end 
+
 local mday={31,28,31,30,31,30,31,31,30,31,30,31}
 
 local function cal()
    local y,m,d,w = os.date("%Y %m %d %w"):split():unpack()
    local mdays = mday[tonumber(m)] + (m == 2 and cgumirs(y) and 1 or 0) 
    local first_index = os.date("%w",os.time({year= y,month =m ,day=1}))
-   local head = " 日 一 二 三 四 五 六 "
+   local head = " Su Mo Tu We Th Fr Sa "
    local str = string.format("        %s-%s-%s\n %s \n",y,m,d,head)
    str = str .. ("   "):rep(first_index)
    for i= 1 ,mdays do
@@ -106,7 +183,7 @@ local function cal()
 end
 local function cowsay(str)
    str = str or cal()
-   local n= cows[ cows.name[ math.random(#cows.name)]]
+   local n= cows[ cowsay_list[ math.random(#cowsay_list)]]
    local max = 0
    local strr = str:split("\n")
       :map(function(elm)
