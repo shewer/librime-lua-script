3266ef0a9633fd26ade72d7a4dac105012052512
Shewer Lu <shewer@gmail.com>
2022-04-17 19:06:33 +0800

    update puts formate
    Signed-off-by: Shewer Lu <shewer@gmail.com>

--- lua/_test.lua
+++ lua/_test.lua
@@ -12,7 +12,7 @@
 local puts=require 'tools/debugtool'
 local M = {}
 local function debug_filter_test()
-  puts(DEBUG,__LINE__(), "-------debug_filter test ")
+  puts(DEBUG, "-------debug_filter test ")
   local schema= Schema("whaleliu_ext")
   local config = schema.config
   assert(schema and tostring(schema):match("Schema") )
@@ -20,33 +20,39 @@ local function debug_filter_test()
 
   local l1=config:get_item("debug_filte")
   assert(li == nil) 
-  puts(DEBUG,__LINE__(), l1)
+  puts(DEBUG, l1)
   local l1=config:get_list("debug_filte")
   assert(li == nil) 
-  puts(DEBUG,__LINE__(), l1)
+  puts(DEBUG, l1)
   local l1=config:get_item("debug_filter")
-  puts(DEBUG,__LINE__(), l1,l1 and l1.type, l)
+  puts(DEBUG, l1,l1 and l1.type, l)
 
   local l1=config:get_item("debug_filter/output_format_str")
-  puts(DEBUG,__LINE__(), l1,l1 and l1.type,l1:get_value().value:gsub("%s",""))
+  puts(DEBUG, l1,l1 and l1.type == "kScalar" and l1:get_value().value:gsub("%s",""))
   local l1=config:get_item("debug_filter/output_format")
-  puts(DEBUG,__LINE__(), l1,l1 and l1.type,l1:get_list().size)
-  puts(DEBUG,__LINE__(), "-------debug_filter test ")
-  assert(false)
+  puts(DEBUG, l1,l1 and l1.type,l1:get_list().size)
+  puts(DEBUG, "-------debug_filter test ")
 end
+function test_Schema_Config()
+  puts(DEBUG, "-----------Schema_Config ----------")
+  local schema=Schema("whaleliu_ext")
+  local config = schema.config
+  assert(schema)
+  assert(config)
+  require 'tools/rime_api'
+
+end
+
 function M.pre_test()
-  puts(DEBUG,__LINE__(),"----------------pre_test start -----------------------")
+  puts(DEBUG,"----------------pre_test start -----------------------")
   local keys=KeySequence()
   keys:parse("abcde")
   assert(keys:repr() == "abcde")
   assert(#keys:toKeyEvent() == 5)
-  local keys=KeySequence("abcde")
-  assert(keys:repr() == "abcde")
-  keys:parse("abcdef")
-  assert(keys:repr() == "abcdef")
 
   debug_filter_test()
-  puts(DEBUG,__LINE__(),"----------------pre_test end -----------------------")
+  test_Schema_Config()
+  puts(DEBUG,"----------------pre_test end -----------------------")
 end
 function M.jjj()
   kkk.a=3
@@ -54,4 +60,5 @@ end
 
 function M.init_test(...)
 end
+M.pre_test()
 return M
