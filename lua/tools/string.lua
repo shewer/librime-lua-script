63dd7855ce61b87ac3597b3b53235daa359f971d
Shewer Lu <shewer@gmail.com>
2022-09-06 21:27:49 +0800

    fixed utf8.sub func
    Signed-off-by: Shewer Lu <shewer@gmail.com>

--- lua/tools/string.lua
+++ lua/tools/string.lua
@@ -25,12 +25,12 @@ end
 
 function utf8.sub(str,si,ei)
   local function index(ustr,i)
-    return i>=0 and ( ustr:utf8_offset(i) or ustr:len() +1 )
-    or ( ustr:utf8_offset(i) or 1 )
+    return i>=0 and ( utf8.offset(ustr,i) or ustr:len() +1 )
+    or ( utf8.offset(ustr,i) or 1 )
   end
 
   local u_si= index(str,si)
-  ei = ei or str:utf8_len()
+  ei = ei or utf8.len(str)
   ei = ei >=0 and ei +1 or ei
   local u_ei= index(str, ei ) -1
   return str:sub(u_si,u_ei)
